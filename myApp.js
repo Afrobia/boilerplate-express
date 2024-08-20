7503d903750a31274294cf08aa6203da3a5a631d
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-08-20 16:06:53 -0300

    use-the--env-file

--- myApp.js
+++ myApp.js
@@ -1,39 +1,29 @@
-let express = require('express');
-let app = express();
+let express = require("express");
+let app = express("");
+require("dotenv").config();
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+/*app.get("/json",(request,response) => {
+  response.json (message);
+});*/
 
+app.get("/json", (req, res) => {
+  const message = process.env.MESSAGE_STYLE;
+  if (message == 'uppercase') {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
