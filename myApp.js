01ec4b24a1321e94cf4547ec3377b1bdf696f511
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-08-20 18:36:04 -0300

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
-let express = require("express");
-let app = express("");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express("");
 require("dotenv").config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 console.log("Hello World");
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.send("Hello Express");
-});
+}); */
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/view/index.html");
@@ -19,11 +24,46 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   const message = process.env.MESSAGE_STYLE;
-  if (message == 'uppercase') {
+  if (message == "uppercase") {
     res.json({ message: "HELLO JSON" });
   } else {
     res.json({ message: "Hello json" });
   }
 });
+//Chain Middleware to Create a Time Server
+
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+/** Chaining middleware. A Time server */
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({ echo: word });
+});
+
+app
+  .route("/name")
+  .get((req, res) => {
+    var first = req.query.first;
+    var last = req.query.last;
+    const jsonObj = { name: `${first} ${last}` };
+    res.send(jsonObj);
+  })
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  });
 
 module.exports = app;
